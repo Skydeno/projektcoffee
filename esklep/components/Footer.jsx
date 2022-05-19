c5bdcd3d7cc2f9631fa8dc11304f4fc70be95a1b
Skydeno <konrad.pietralski@gmail.com>
2022-05-19 11:31:37 +0200

    Pierwszy deploy

---
+++ esklep/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter, AiFillFacebook } from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+      <p>Projekt na zaliczenie - sklep z kawą 2022</p>
+      <p className='icons'>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+        <AiFillFacebook />
+      </p>
+    </div>
+  )
+}
+
+
+export default Footer
---
+++ esklep/components/Layout.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Head from 'next/head';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
+  return (
+    <div className='layout'>
+      <Head>
+        <title>Projekt na zaliczenie - sklep z kawą 2022</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>
+        {children}
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
